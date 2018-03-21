1ecaae2583e4a798964ed288b7a6dc9f42d9c53f
Rochet2 <tqbattlenet@gmail.com>
2018-03-21 18:27:57 +0200

    Add comments

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -7,27 +7,38 @@ namespace Interpreter
     class MainClass
     {
         /*
-         * A function used to run the program as a whole
+         * A function used to run the interpreter as a whole
          * with given IO and InputBuffer.
          */
         public static void Run(IO io, InputBuffer input)
         {
+            // Lexical analysis
             Lexer lexer = new Lexer(input, io);
-            lexer.LexAll();
+            lexer.LexAll(); // tokenize whole input to get all lexical errors immediately
+
+            // Parse to create the AST
             Parser parser = new Parser(lexer, io);
             ASTNode ast = parser.Parse();
             if (lexer.errored || parser.errored)
-                return;
+                return; // Parser or lexer had errors, exit
+
+            // Pefrorm semantic analysis
             Analysis semanticanalysis = new Analysis(ast, io);
             semanticanalysis.Visit();
             if (semanticanalysis.errored)
-                return;
-            Interpreter inter = new Interpreter(ast, io);
-            inter.Visit();
-            if (inter.errored)
+                return; // Semantic analysis failed, exit
+
+            // Run the interpreter
+            Interpreter interpreter = new Interpreter(ast, io);
+            interpreter.Visit(); // Run interpreter
+            if (interpreter.errored)
                 io.WriteLine("Interpreter terminated with errors");
         }
 
+        /*
+         * The entry point of the program.
+         * Handles input parameters.
+         */
         public static void Main(string[] args)
         {
             IO io = new IOConsole();
@@ -39,15 +50,17 @@ namespace Interpreter
             }
             try
             {
+                // Try read input file and run with it as the input
                 using (FileStream fileStream = File.Open(args[0], FileMode.Open))
                 {
-                    InputBuffer input = new InputBuffer(fileStream, 2);
+                    int bufferSize = 2;
+                    InputBuffer input = new InputBuffer(fileStream, bufferSize);
                     Run(io, input);
                 }
             }
             catch (Exception e)
             {
-                // log any file opening exceptions
+                // log any exceptions, for example related to file opening
                 io.WriteLine("There were problems with using file {0}:", args[0]);
                 io.WriteLine(e.ToString());
             }
